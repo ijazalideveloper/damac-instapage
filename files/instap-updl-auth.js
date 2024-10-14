2c0fe757466a07dadb5b95cba85b003a7319ef9c
Ijaz Ali <v-ijaz@damacgroup.com>
2024-10-14 10:49:47 +0400

    updated

--- files/instap-updl-auth.js
+++ files/instap-updl-auth.js
@@ -2361,7 +2361,8 @@ function getFormData($form) {
 
     indexed_array[n["name"]] = n["value"];
   });
-  console.log('indexed_array...', indexed_array, delete indexed_array[campaignName]);
+  console.log(delete indexed_array[campaignName]);
+  console.log('indexed_array...', indexed_array)
   return indexed_array;
 }
 
