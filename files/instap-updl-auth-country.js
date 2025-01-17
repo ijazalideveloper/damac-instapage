3f930f2a26287f840b74a264db1d837e58e5b767
Ijaz Ali <v-ijaz@damacgroup.com>
2025-01-17 12:16:28 +0400

    country issue fixed

--- files/instap-updl-auth-country.js
+++ files/instap-updl-auth-country.js
@@ -2068,7 +2068,8 @@ window.addEventListener("DOMContentLoaded", function () {
           .name.replace(/ *\([^)]*\) */g, "");
 
         $("input[name='countryCode']").val(retrieveCountry(selectedCountryName)?.sendAs?.countryCode);
-        $("input[name='country']").val(decodeURIComponent(retrieveCountry(selectedCountryName)?.sendAs?.country));
+        $("input[name='country']").val();
+        // $("input[name='country']").val(decodeURIComponent(retrieveCountry(selectedCountryName)?.sendAs?.country));
         $("input[name='ga_client_id']").val(getCookie("_ga"));
         $("input[name='fbid']").val(getFacebookCookie("_fbp"));
         $("input[name='fbclid']").val(getFbc());
