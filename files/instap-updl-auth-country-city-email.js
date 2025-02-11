28e7052aec24353239ae5320d920f483f2d8f4d0
Ijaz Ali <v-ijaz@damacgroup.com>
2025-02-11 11:13:36 +0400

    commented the code at the end of the page for testing

--- files/instap-updl-auth-country-city-email.js
+++ files/instap-updl-auth-country-city-email.js
@@ -2119,7 +2119,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
                 if (citiesByCountry[selectedCountry]) {
                     cityDropdown.style.display = "block"; // Show city dropdown
-                    cityDropdown.appendChild(new Option("Select City", "", true, true));
+                    // cityDropdown.appendChild(new Option("Select City", "", true, true));
 
                     citiesByCountry[selectedCountry].forEach(city => {
                         let option = new Option(city, city.toLowerCase());
